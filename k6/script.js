562230bce45f73acffb4e358f102eaeae0191bea
RRFreitas <rennanfreitas11@gmail.com>
2023-10-14 08:37:59 -0300

    Refatorando script

--- k6/script.js
+++ k6/script.js
@@ -34,19 +34,6 @@ const product = {
 
 export function setup() {
   http.post(`${url}/users`, payload, params); // create user if it not exists
-  const products = http.get(`${url}/products`)
-
-  const falta = 100-products.length;
-  for(let i = 0; i < falta; i++) {
-    http.post(`${url}/products`, product, params);
-    const res2 = http.post(`${url}/products`, payload, params);
-    check(res2, {
-      'POST products - status is 200': (r) => r.status === 200,
-    });
-  }
-}
-
-export default function () {
   const res = http.post(`${url}/users/login`, payload, params);
   check(res, {
     'login - status is 200': (r) => r.status === 200,
@@ -54,15 +41,28 @@ export default function () {
   });
   const token = res.json()["token"]
   authParams['headers']['Authorization'] = `Bearer ${token}`;
+
   const products = http.get(`${url}/products`, authParams);
-  check(products, {
+  const falta = 10-products.json().length;
+
+  for(let i = 0; i < falta; i++) {
+    const res2 = http.post(`${url}/products`, JSON.stringify(product), authParams);
+    check(res2, {
+      'POST products - status is 200': (r) => r.status === 200,
+    });
+  }
+  const products2 = http.get(`${url}/products`, authParams);
+  check(products2, {
     'at least one product': (r) => r.json().length > 0
   })
+  return {token: token, product_id: products2.json()[0]._id};
+}
 
+export default function (data) {
+  authParams['headers']['Authorization'] = `Bearer ${data['token']}`;
   const buy_res = http.post(`${url}/products/buy`, JSON.stringify({
-    ids: [products.json()[0]._id]
+    ids: [data['product_id']]
   }), authParams);
-  console.log(buy_res)
   check(buy_res, {
     'buy - status is 201': (r) => r.status === 201,
     'buy is completed': (r) => r.json().status === "completed",
